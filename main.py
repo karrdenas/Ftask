30e6ca8c0db138b86be1df5b32d971647ae52eb6
Alexander <karrdenas@gmail.com>
2021-02-28 17:25:29 +0300

    first

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+import string
+
+btns_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def ansbut(a):
+    but + str(a) = random.choice(buttons)
+    buttons.remove(but)
+    print(but)
+
+
+for a in range(3):
+    ansbut(a)
+    '''
+    but1 = random.choice(btn_list)
+    btns_list.remove(but1)
+    print(but1)
+
+    but2 = random.choice(btn_list)
+    btns_list.remove(but2)
+    print(but2)
+
+    but3 = random.choice(btn_list)
+    btns_list.remove(but3)
+    print(but3)
+    
+    То, что должна делать функция
+    '''
+
